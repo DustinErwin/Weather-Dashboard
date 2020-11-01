f59b1b91e29d30facf4834191570bb9d415796d3
Galahad <derwin499@gmail.com>
2020-11-01 00:51:32 -0400

    changed math for forecast

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
 let loN = 0;
 let laT = 0;
 let uvReturn;
-let month;
-let day;
-let searchNum = 1
+let call;
+let searchNum = 1 + localStorage.length
 let months = [
   null,
   "January",
@@ -19,13 +18,16 @@ let months = [
   "Novemeber",
   "December",
 ];
-let queryCity = 'boston'
+let queryCity = localStorage.getItem('mostRecent')
+
 let wthrTodayApi =
   "https://api.openweathermap.org/data/2.5/forecast?q=" +
   queryCity +
   "&units=imperial&appid=b15fbded32e97830c7c19b58cb5acfd0";
 
-
+getAPI()
+//Grabs API data
+function getAPI(){
   $.ajax({
     url: wthrTodayApi,
     method: "GET",
@@ -35,19 +37,32 @@ let wthrTodayApi =
     laT = call.city.coord.lat.toString(10);
     loN = call.city.coord.lon.toString(10);
 
+   mainInfo()
+
+    uvFind(laT, loN);
+
+    
+
+  })};
+
+  //Creates the data to fill main info panel
+  function mainInfo () {
     name = call.city.name;
 
     dateIndex = call.list[0].dt_txt.substring(0, 11);
     d = dateIndex.substring(8, 10);
     m = dateIndex.substring(5, 7);
     y = dateIndex.substring(0, 4);
-    day = daySuff(parseInt(d));
+    day = daySuffix(parseInt(d));
     month = monthName(parseInt(m));
     date = `${month} ${day} ${y}`;
 
     temp = parseInt(call.list[0].main.temp);
+
     humid = call.list[0].main.humidity;
+
     wind = parseInt(call.list[0].wind.speed * 2.237);
+
     let icon = new Image(40, 40);
     icon.src =
       "http://openweathermap.org/img/wn/" +
@@ -55,38 +70,41 @@ let wthrTodayApi =
       "@2x.png";
 
     weatherDesc = call.list[0].weather[0].main;
-
-  for (let i = 7;i >= 39;i + 8) {
-    dateIndex = call.list[i].dt_txt.substring(0, 11);
-    d = dateIndex.substring(8, 10);
-    m = dateIndex.substring(5, 7);
-    day = daySuff(parseInt(d));
-    month = monthName(parseInt(m));
-    date = `${month} ${day}`;
-    $("#date-" + i).empty();
-    $("#date-" + i).text(date);
-    temp = parseInt(call.list[i].main.temp);
-    $("#temp-" + i).empty();
-    console.log(temp)
-    $("#temp-" + i).text("Temp: " + temp + "°F");
-    humid = call.list[i].main.humidity;
-    $("#humid-" + i).empty();
-    console.log(humid)
-    $("#humid-" + i).text("Humidity: " + humid + "%");
-  }
-
-    uvFind(laT, loN);
-
+    
+    fillForecast()
+     
     mainTile(name, date, temp, humid, wind, icon, weatherDesc);
-    //  fillForecast(call);
-  });
-
-
-
-  for (let i = 1;i <= 9;i++){
-    rc = localStorage.getItem('city' + i)
-    $('.history').append(`<button id="btn-${i}" type='button' class="btn btn-danger histBtn">${rc}</button>`)
   }
+
+  function fillForecast() {
+    for (let i = 1;i <= 4;i++) {
+      dateIndex = call.list[i * 8].dt_txt.substring(0, 11);
+      d = dateIndex.substring(8, 10);
+      m = dateIndex.substring(5, 7);
+      day = daySuffix(parseInt(d));
+      month = monthName(parseInt(m));
+      date = `${month} ${day}`;
+      $("#date-" + i).text(date);
+
+      desc = call.list[i *8].weather[0].main
+       $("#sky-" + i).text(desc);
+
+      temp = parseInt(call.list[i * 8].main.temp);
+            $("#temp-" + i).text("Temp: " + temp + "°F");
+
+      humid = call.list[i * 8].main.humidity;      
+      $("#humid-" + i).text("Humidity: " + humid + "%");
+    }}
+
+  popButtons()
+
+ function popButtons(){
+  $('.history').empty()
+  for (let i = 1;i < localStorage.length&&i <= 9;i++){
+    rc = localStorage.getItem('city' + i)
+    
+    $('.history').append(`<button id="btn-${i}" onclick="reSearch(${i})" type='button' class="btn btn-danger histBtn">${rc}</button>`)
+  }}
  
 
 
@@ -109,10 +127,11 @@ function uvFind(a, b) {
     uvReturn = response.value.toString(10);
     uvText = $("#uvIndex");
     uvText.empty();
-    console.log(uvReturn);
+   
     uvText.text(uvReturn);
   });
 }
+
 function uvAlert(a) {
   if (a >= 3 && a <= 5) {
     $("#uvIndex").css("background-color", "yellow");
@@ -129,7 +148,7 @@ function uvAlert(a) {
 
 
 
-function daySuff(a) {
+function daySuffix(a) {
   if (a === 1 || a === 21 || a === 31) {
     let day = a.toString(10) + "st";
     return day;
@@ -153,27 +172,45 @@ function monthName(a) {
 function mainTile(a, b, c, d, e, f, g) {
   cityHeader = $("#cityHeader").empty();
   cityHeader.text(a);
+
   cityDate = $("#cityDate").empty();
   cityDate.text(b);
+
   temp = $("#temp").empty();
   temp.text(c);
+
   humid = $("#humid").empty();
   humid.text(d);
+
   wind = $("#wind").empty();
   wind.text(e);
+
+  $("#desc").append(f);
+
   desc = $("#desc").empty();
   desc.text(g);
-  $("#desc").append(f);
+  
+  
 }
 
 function storSearch(a, b) {
-  
   localStorage.setItem(a + searchNum, b);
   searchNum++
   localStorage.setItem('mostRecent', b)
 }
 
+function reSearch(e) {
+  reQuery = localStorage.getItem('city' + e)
+ 
+  queryCity = reQuery
+  getAPI()
+}
+
 $("#btn-0").click(function(){
-  queryCity = $('#queryCity').val()
+  queryCity = $('#queryCity').val() 
+  if(queryCity){
+  getAPI()
   storSearch('city', queryCity)
+  popButtons()
+  }
 });
